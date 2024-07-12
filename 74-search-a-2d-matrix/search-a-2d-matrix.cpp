a94a2c898660daeea0a935d806336de4e737e4ab
haris71rc <113758103+haris71rc@users.noreply.github.com>
2024-07-13 00:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (10.52%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,33 +1,24 @@
 class Solution {
-private:
-    bool binarySearch(int i,vector<vector<int>>& matrix,int target){
-        int start=0;
-        int end=matrix[0].size()-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(matrix[i][mid]>target){
-                end=mid-1;
+   
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int left=0;
+        int right= n*m-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            int midvalue = matrix[mid/m][mid%m];
+            if(midvalue>target){
+                right=mid-1;
             }
-            else if(matrix[i][mid]<target){
-                start=mid+1;
+            else if(midvalue<target){
+                left=mid+1;
             }
             else{
                 return true;
             }
         }
         return false;
-    }   
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        bool ans=false;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target && matrix[i][m-1]>=target){
-                cout<<i;
-                ans=binarySearch(i,matrix,target);
-            }
-        }
-        return ans;
     }
 };
