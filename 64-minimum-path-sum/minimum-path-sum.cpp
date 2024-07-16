31a914c20ca0bd78ac015b4e30aeba9ca74a29b7
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2024-07-17 02:23:34 +0530

    Time: 10 ms (26.05%) | Memory: 12.7 MB (19.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -13,12 +13,33 @@ private:
         
         return dp[i][j]=grid[i][j] + min(right, down);
     }
+    int solveMemo(vector<vector<int>>& grid,int n,int m){
+        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
+        dp[n-1][m-1]=grid[n-1][m-1];
+        for (int j = m-2; j >= 0; --j) {
+            dp[n-1][j] = grid[n-1][j] + dp[n-1][j+1];
+        }
+        
+        // Fill the last column (can only move down)
+        for (int i = n-2; i >= 0; --i) {
+            dp[i][m-1] = grid[i][m-1] + dp[i+1][m-1];
+        }
+        for(int i=n-2;i>=0;--i){
+            for(int j=m-2;j>=0;--j){
+                int right = dp[i][j+1];
+                int down = dp[i+1][j];
+        
+                dp[i][j]=grid[i][j] + min(right, down);
+            }
+        }
+        return dp[0][0];
+    }
   
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(grid,0,0,n,m,dp);
+        //vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solveMemo(grid,n,m);
     }
 };
