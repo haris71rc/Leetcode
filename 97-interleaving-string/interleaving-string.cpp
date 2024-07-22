2975f43367b04d8e56092ed419de77605ccb0996
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2024-07-23 02:59:19 +0530

    Time: 35 ms (21.46%) | Memory: 108.5 MB (6.01%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
-public:
-    int rec(int i1,int i2,int i3,string s1, string s2, string s3,vector<vector<vector<int>>>&dp){
-        if(i1==s1.size()&&i2==s2.size()&&i3==s3.size()){
+private:
+    int solve(string s1, string s2, string s3,int i,int j,int k,vector<vector<vector<int>>> &dp){
+        if(k==s3.length() && i==s1.length() && j==s2.length()){
             return 1;
         }
-        if(i3==s3.size())return 0;
-        if(dp[i1][i2][i3]!=-1)return dp[i1][i2][i3];
+        if(k==s3.length()){
+            return 0;
+        }
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
         int a=0,b=0;
-        if(i1<s1.size() && s1[i1]==s3[i3]){
-            a=rec(i1+1,i2,i3+1,s1,s2,s3,dp);
+        if(i<s1.length() && s1[i]==s3[k]){
+             a= solve(s1,s2,s3,i+1,j,k+1,dp);
         }
-        if(i2<s2.size() && s2[i2]==s3[i3]){
-            b=rec(i1,i2+1,i3+1,s1,s2,s3,dp);
+        if(j<s2.length() && s2[j]==s3[k]){
+            b= solve(s1,s2,s3,i,j+1,k+1,dp);
         }
-        return dp[i1][i2][i3]=a||b;
-    }
+        return dp[i][j][k]=a||b;
+    }    
+public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n1=s1.size();
-        int n2=s2.size();
-        int n3=s3.size();
-        vector<vector<vector<int>>>dp(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1,-1)));
-        return rec(0,0,0,s1,s2,s3,dp);
+        vector<vector<vector<int>>>dp(s1.length()+1,vector<vector<int>>(s2.length()+1,vector<int>(s3.length()+1,-1)));
+        return solve(s1,s2,s3,0,0,0,dp);
     }
 };
