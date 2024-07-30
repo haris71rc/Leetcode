3d6d01924e6bc03ff40c675af0aa143b7d10ab08
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2024-07-31 00:47:54 +0530

    Time: 11 ms (37.01%) | Memory: 9.9 MB (14.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> m;
-        for(int i=0;i<s.size();i++){
+        map<char, int> m;
+        if(s.size()!=t.size()) return false;
+        for (int i = 0; i < s.size(); i++) {
             m[s[i]]++;
+            m[t[i]]--;
         }
-        for(int i=0;i<t.size();i++){
-                m[t[i]]--;
-                    }
-        for(auto i:m){
-            if(i.second!=0) return false;
+        for (auto i : m) {
+            if (i.second != 0)
+                return false;
         }
         return true;
     }
