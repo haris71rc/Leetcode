ab4de8f21b839f4abef8d0e77c8c824333ea59f8
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2026-05-19 00:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.37%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,24 +1,23 @@
 class Solution {
-   
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int left=0;
-        int right= n*m-1;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            int midvalue = matrix[mid/m][mid%m];
-            if(midvalue>target){
-                right=mid-1;
-            }
-            else if(midvalue<target){
-                left=mid+1;
-            }
-            else{
-                return true;
-            }
+       int n = matrix.size(), m = matrix[0].size();
+       int left=0, right=n*m-1;
+
+       while(left<=right){
+        int mid = left + (right-left)/2;
+        int midValue = matrix[mid/m][mid%m];
+
+        if(midValue<target){
+            left = mid+1;
         }
-        return false;
+        else if(midValue>target){
+            right = mid-1;
+        }
+        else{
+            return true;
+        }
+       }
+       return false;
     }
 };
