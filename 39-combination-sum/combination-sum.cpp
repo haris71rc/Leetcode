75c0c3b020e87f7290e014abb88ede0d0984674a
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2026-05-23 15:57:16 +0530

    Time: 83 ms (7.68%) | Memory: 72.3 MB (5.70%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 private:
-    void solve(vector<vector<int>>&ans,vector<int> &output,int index,int target,vector<int>& candidates){
-        
-        if(index>=candidates.size() || target<0){
-            return;
-        }
-        if(target==0){
-            ans.push_back(output);
-            return;
-        }
-        
-            output.push_back(candidates[index]);
-            solve(ans,output,index,target-candidates[index],candidates);
-            output.pop_back();
-            solve(ans,output,index+1,target,candidates);
-            
-        
-    }    
+    void solve(vector<vector<int>> &ans, int index, vector<int> output, vector<int>& candidates, int target){
+       if(index>=candidates.size() || target<0){
+        return;
+       } 
+       if(target==0){
+        ans.push_back(output);
+        return;
+       }
+
+       output.push_back(candidates[index]);
+       solve(ans,index,output,candidates,target-candidates[index]);
+       output.pop_back();
+       solve(ans,index+1,output,candidates,target);
+
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
+        vector<vector<int>> ans;
+        int index=0;
         vector<int> output;
-        solve(ans,output,0,target,candidates);
+
+        solve(ans,index,output,candidates,target);
+
         return ans;
     }
 };
