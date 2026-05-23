f8006867121190e7783bfd5cfff2a9ef65e40ea2
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2026-05-23 20:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.90%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,25 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-      vector<vector<int>> ans;
-      if(root==NULL) return ans;
-      queue<TreeNode*> q;
-      bool leftToRight=true;
-      q.push(root);
-      while(!q.empty()){
-        int size=q.size();
-        vector<int> v(size);
-        for(int i=0;i<size;i++){
-            TreeNode* temp=q.front();
-            q.pop();
-            int index = leftToRight? i: size-i-1;
-            v[index]=temp->val;
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        bool ifLR = false;
+
+        while(!q.empty()){
+            int s = q.size();
+            vector<int> v;
+
+            for(int i=0;i<s;i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
+                v.push_back(curr->val);                
+            }
+            ifLR = !ifLR;
+            if(ifLR){
+                ans.push_back(v);}
+            
+            else{
+                reverse(v.begin(),v.end());
+                ans.push_back(v);
+            }
         }
-        leftToRight=!leftToRight;
-        ans.push_back(v);
-      }  
-      return ans;
+        return ans;
     }
 };
