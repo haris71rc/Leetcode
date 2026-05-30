e9014de37c9214350bacd614d84609049372ad17
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2026-05-30 23:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (68.70%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,25 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    void putInV(TreeNode* root,vector<int> &v,int digits){
-        if(root->right==NULL && root->left==NULL){
-            digits=digits*10+root->val;
-            v.push_back(digits);
-            return;
+private: 
+    void solve(TreeNode* root,int &count,int currSum){
+        if(root==NULL) return;
+        currSum = currSum*10 + root->val;
+        if(root->left==NULL && root->right==NULL){
+            count+=currSum;
         }
 
-        digits=digits*10+root->val;
-        if(root->left){putInV(root->left,v,digits);}
-        if(root->right){putInV(root->right,v,digits);}
-        
-    }    
+        solve(root->left, count, currSum);
+        solve(root->right, count, currSum);
+
+        currSum-=root->val;
+    }
 public:
     int sumNumbers(TreeNode* root) {
-        vector<int> v;
-        putInV(root,v,0); 
-        int sum=0;
-        for(auto i:v){
-            sum+=i;
-        }      
-        return sum;
+        int count=0;
+        solve(root,count,0);
+        return count;
     }
 };
