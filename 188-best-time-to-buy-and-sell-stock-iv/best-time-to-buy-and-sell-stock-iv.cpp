06d00c44be6c1d6bfa602dccd16b8275419d5fe7
Mohd Haris <113758103+haris71rc@users.noreply.github.com>
2026-06-11 23:49:59 +0530

    Time: 19 ms (17.37%) | Memory: 18.3 MB (27.80%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,33 +1,29 @@
 class Solution {
-    private:
-           int solveMemo(vector<int>& prices,int i,int buy,int limit,vector<vector<vector<int>>>& dp){
-        if(i>=prices.size()){
-            return 0;
-        }
-        if(limit==0){
-            return 0;
-        }
-        if(dp[i][buy][limit]!=-1){
-            return dp[i][buy][limit];
-        }
+private:
+    int solve(vector<int>& prices, int i, int buy, int trans, vector<vector<vector<int>>> &dp,int k){
+        if(i>=prices.size()) return 0;
+        if(trans==k) return 0;
+
+        int ans=0;
+
+        if(dp[i][buy][trans]!=-1) return dp[i][buy][trans];
+
         if(buy){
-            int buyKro = -prices[i]+solveMemo(prices,i+1,0,limit,dp);
-            int skipKro = 0+solveMemo(prices,i+1,1,limit,dp);
-            dp[i][buy][limit] = max(buyKro,skipKro);
+            int buy =- prices[i] + solve(prices,i+1,0,trans,dp,k);
+            int skip= solve(prices,i+1,1,trans,dp,k);
+            ans=max(buy,skip);
         }
         else{
-            int sellKro = prices[i]+solveMemo(prices,i+1,1,limit-1,dp);
-            int skipKro = 0+solveMemo(prices,i+1,0,limit,dp);
-            dp[i][buy][limit] = max(sellKro,skipKro);
+            int sell =+ prices[i] + solve(prices,i+1,1,trans+1,dp,k);
+            int skip= solve(prices,i+1,0,trans,dp,k);
+            ans=max(sell,skip);
         }
-        return dp[i][buy][limit];
-    } 
+        return dp[i][buy][trans] = ans;
+    }
 public:
     int maxProfit(int k, vector<int>& prices) {
-                int n=prices.size();
-
-         vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-         return solveMemo(prices,0,1,k,dp);
-       
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k,-1)));
+        return solve(prices,0,1,0,dp,k);
     }
 };
